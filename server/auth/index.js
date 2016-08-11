18bbde27f530bde73b774aa9e62f35c8f1525975
Si Wai Joanne <ngsiwai@gmail.com>
2016-08-12 01:54:22 +0800

    refactor

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,19 +1,17 @@
-'use strict';
-
-var express = require('express');
-var passport = require('passport');
-var config = require('../config/environment');
-var User = require('../api/user/user.model');
+var express = require('express')
+var passport = require('passport')
+var config = require('../config/environment')
+var User = require('../api/user/user.model')
 
 // Passport Configuration
-require('./local/passport').setup(User, config);
-require('./facebook/passport').setup(User, config);
-require('./twitter/passport').setup(User, config);
+require('./local/passport').setup(User, config)
+require('./facebook/passport').setup(User, config)
+require('./twitter/passport').setup(User, config)
 
-var router = express.Router();
+var router = express.Router()
 
-router.use('/local', require('./local'));
-router.use('/facebook', require('./facebook'));
-router.use('/twitter', require('./twitter'));
+router.use('/local', require('./local'))
+router.use('/facebook', require('./facebook'))
+router.use('/twitter', require('./twitter'))
 
-module.exports = router;
+module.exports = router
